78facea225ef1cf3a003a2938a88874477db27a9
PR7175Z <aadimpraz@gmail.com>
2024-02-28 18:01:42 +0545

    After a lot of changes in every file

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
-root = Tk()
-root.title("Music Player")
-root.geometry("350x500")
-# icon for the app
-root.iconbitmap(r'D:\pythonlectures\semprojectraw\assets\images\logo.ico')
-frm = ttk.Frame(root, padding=(70,20,0,0))
-frm.place(relx=0.5, rely=0.5, anchor=CENTER)
-frm.grid()
+import streamlit as st
+import streamlit.components.v1 as components
+from streamlit.components.v1 import html
+import base64
+from function import stream, stop
 
-def enterClicked():
-    searchquery.get()
-    ttk.Label(frm, text="", textvariable=showsearchquery).grid(column=0, row=1)
-    showsearchquery.set('Searching for ' + searchquery.get())
+def gifload(path):
+    file_ = open("assets/images/music.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    return data_url
 
-Progress_Bar=ttk.Progressbar(frm,orient=HORIZONTAL,length=250,mode='determinate')
+st.markdown(
+    '''
+    <style>
+        button[title="View fullscreen"]{
+            visibility: hidden;
+        }
+        .gifimg{
+            width: 500px;
+        }
+    </style>
+    ''',unsafe_allow_html=True
+)
 
-def Slide():
-    import time
-    Progress_Bar['value'] =0
-    for x in range(0, 100):
-        Progress_Bar['value']+=x
-        frm.update_idletasks()
-        time.sleep(0.1)
+row1col1, row1col2 = st.columns([0.1, 0.9])
+with row1col1:
+    st.image("assets/images/logo.png", width=64)
+with row1col2:
+    st.markdown('<h1 class="pagetitle">Music Player</h1>',
+                unsafe_allow_html=True)
 
-Progress_Bar.grid(row=0, column=0)
-Button(frm,text='Run',command=Slide).grid(row=0, column=1)
+with st.form('my_form'):
+    row2col1, row2col2 = st.columns([0.8, 0.2])
+    with row2col1:
+        inp = st.text_input(' ', placeholder='Search Here...')
+    with row2col2:
+        submitted = st.form_submit_button('Search')
 
-searchquery = tk.StringVar()
-showsearchquery = tk.StringVar()
-# entry_box = ttk.Entry(frm, textvariable=searchquery)
-# entry_box.grid(column=0, row=0)
-# enterbtn = ttk.Button(frm, text="Enter", command=enterClicked)
-# enterbtn.grid(column=12, row=0)
-# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=12, row=1)
-root.mainloop()
+if submitted:
+    stream(inp)
+    stopbtn = st.button('Stop')
+
+    if stopbtn:
+        stop()
+
+# file_ = open("assets/images/music.gif", "rb")
+# contents = file_.read()
+# data_url = base64.b64encode(contents).decode("utf-8")
+# file_.close()
+
+data_url = gifload("assets/images/music.gif")
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" class="gifimg" alt="musicgif">',
+    unsafe_allow_html=True,
+)
+    
