7f43ddeee8a0b06c728a41bda5144901954dc5f7
PR7175Z <aadimpraz@gmail.com>
2024-03-26 23:31:30 +0545

    added extension file in include

--- seleniumfn.py
+++ seleniumfn.py
@@ -11,24 +11,31 @@ options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
 options.add_argument("--headless") 
 options.add_argument("--log-level") 
 options.add_argument('--disable-application-cache') 
-ser = Service("D:\\pythonlectures\\semprojectraw\\include\\geckodriver.exe") 
-driver = webdriver.Firefox(service=ser, options=options) 
-# driver = webdriver.Firefox(options=options) 
+# ser = Service("D:\\pythonlectures\\semprojectraw\\include\\geckodriver.exe") 
+# driver = webdriver.Firefox(service=ser, options=options) 
+driver = webdriver.Firefox(options=options) 
 
-extension1 = "C://Users//user//Downloads//ublock_origin-1.55.0.xpi"
+extension1 = "D://pythonlectures//semprojectraw//include//ublock_origin-1.55.0.xpi"
 driver.install_addon(extension1, temporary=True) 
 
 def stream(MusicName):
 	driver.get(f"https://www.youtube.com/results?search_query={MusicName}") 
 	driver.implicitly_wait(0.5) 
-	print(driver.session_id == 'None')
+	# print(driver.session_id == 'None')
 	video = driver.find_element( 
 		"xpath", "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/div/div[1]/div/h3/a/yt-formatted-string") 
 	video.click()
 	return True
 
+def pauseAndPlay(): 
+    pauseVideo = driver.find_element( 
+        "xpath", "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[1]/video") 
+    driver.implicitly_wait(1) 
+    pauseVideo.click() 
+
 def stop(): 
 	driver.quit() 
+	exit()
 
 # we need to use Stream {songname} for our player to stream the song 
 if __name__ == "__main__": 
@@ -40,7 +47,9 @@ if __name__ == "__main__":
 			if uinput.split(" ", 1)[0] == "st": 
 				songName = uinput.split(" ", 1)[1] 
 				print(songName)
-				stream(songName) 
+				stream(songName)
+			elif uinput == "play":
+				pauseAndPlay()
 			elif uinput == "stop": 
 				stop() 
 
